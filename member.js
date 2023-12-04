6bbc930458635d0e63cacf370dfa1cf62d59f313
Aldair Mendoza <106117317+Aldair-MendozaC@users.noreply.github.com>
2023-12-04 18:01:44 +0000

    Copilot second commit proof

--- member.js
+++ member.js
@@ -1,14 +1,8 @@
 function skillsMember() {
-  var skills = ['html', 'css', 'javascript', 'react', 'nodejs'];
-  var member = {
-    name: 'Diana',
-    age: 23,
-    skills: skills
+  return {
+    restrict: 'E',
+    templateUrl: 'modules/skills/views/skills-member.html',
+    controller: 'SkillsController',
+    controllerAs: 'skillsCtrl'
   };
-  // Write code here
-  var newSkills = member.skills;
-  newSkills.push('firebase');
-  console.log(newSkills);
-  // Don’t change the code below
-  return member;
 }
